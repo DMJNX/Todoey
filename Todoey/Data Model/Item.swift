4e5303d8206121274e07c364516279bd53f6cd27
Damian Jasinowski <damianjasinowski@icloud.com>
2021-01-13 15:56:34 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Damian Jasinowski on 08/01/2021.
+//
+
+import Foundation
+
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
